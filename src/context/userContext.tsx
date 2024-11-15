bac6163786afa632c00f8cc63b069346d3b5def9
Akib <akibderaiya123@gmail.com>
2024-11-15 16:36:46 +0530

    initial commit

---
+++ src/context/userContext.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+
+// Define the type for the product context
+interface UserContextType {
+	currentUser: any | null;
+	setCurrentUser: (arg: any) => void;
+	logOutUser: () => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+interface UserProviderProps {
+	children: ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState<any | null>(null);
+
+	useEffect(() => {
+		const user = localStorage.getItem('admin')
+		if (user) {
+			setCurrentUser(JSON.parse(user))
+		}
+	}, [])
+
+	const logOutUser = () => {
+		setCurrentUser(null)
+		localStorage.removeItem('admin')
+		localStorage.clear()
+	}
+
+	return (
+		<UserContext.Provider value={{ currentUser, setCurrentUser, logOutUser }}>
+			{children}
+		</UserContext.Provider>
+	);
+};
+
+export const useCurrentUser = () => {
+	const context = useContext(UserContext);
+	if (!context) {
+		throw new Error('useProduct must be used within a UserProvider');
+	}
+	return context;
+};
